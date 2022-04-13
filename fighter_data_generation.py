49305c87b17ef3f493f58a81cc2ac3f57ce5f134
Michal <maksenczuk@gmail.com>
2022-04-13 19:44:56 +0100

    first commit

---
+++ fighter_data_generation.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_columns', 100)
+
+df = pd.read_csv("datasets/UFC_processed.csv")
+
+df["date"] = pd.to_datetime(df["date"])
+
+features = ["date","fighter"]
+for name in df.columns[4:28]: # slice to get rid of R_ & B_
+    features.append(name[2:])
+
+# separate fighters
+rFighter = pd.concat([df.iloc[:,[0,1,4]],df.iloc[:,28:]],axis=1)
+bFighter = pd.concat([df.iloc[:,[0,2,4]],df.iloc[:,5:28]],axis=1)
+
+# rename columns
+rFighter.columns = features
+bFighter.columns = features
+
+# rFighter & bFighter concat in one dataframe
+fighters = pd.concat([rFighter,bFighter],axis=0,).reset_index(drop=True)
+
+# group to get details from fighters latest fight
+groupedFighter = fighters.groupby("fighter")
+
+# group by fighters name to get all their fights
+# sort by date to get their latest fight
+# get their latest fight with iloc[0]
+fighters_detail = []
+for fighter in fighters["fighter"].unique(): 
+    fighters_detail.append(groupedFighter.get_group(fighter).sort_values(by=["date"],ascending=False).iloc[0])
+
+print(fighters_detail[0])
+fighter_stat = pd.DataFrame(fighters_detail).sort_values(by="fighter")
+fighter_stat.insert(0, 'ID', np.arange(1,len(fighter_stat.index)+1))
+fighter_stat.reset_index(drop=True, inplace=True)
+
+# export dataset
+fighter_stat.to_csv("datasets/FIGHTER_STAT.csv",index=False)
---
+++ preprocessing.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import os
+
+SEED = 42
+os.environ['PYTHONHASHSEED'] = str(SEED)
+np.random.seed(SEED)
+pd.set_option("display.max_columns", 500)
+pd.set_option("display.max_rows", 50)
+
+
+df = pd.read_csv("datasets/ufc-master.csv")
+print(df.shape)
+
+df = df[df.columns.drop(list(df.filter(regex="odds")))]
+df = df[df.columns.drop(list(df.filter(regex="ev")))]
+df = df[df.columns.drop(list(df.filter(regex="_rank")))]
+df = df[df.columns.drop(list(df.filter(regex="finish")))]		
+df = df[df.columns.drop(list(df.filter(regex="total_fight_time_secs")))]
+df = df[df.columns.drop(list(df.filter(regex="dif")))]	
+df = df[df.columns.drop(list(df.filter(regex="constant_1")))]	
+df = df[df.columns.drop(list(df.filter(regex="empty_arena")))]
+df = df[df.columns.drop(list(df.filter(regex="location")))]
+df = df[df.columns.drop(list(df.filter(regex="title_bout")))]
+df = df[df.columns.drop(list(df.filter(regex="gender")))]
+df = df[df.columns.drop(list(df.filter(regex="country")))]
+df = df[df.columns.drop(list(df.filter(regex="no_of_rounds")))]					
+
+df["date"] = pd.to_datetime(df["date"]) # date to datetime
+
+df = df.replace(r'^\s*$', np.nan, regex=True) # replace empty strings with nan
+df = df.fillna(np.nan) # fill empty and nans with nan
+
+dateColumn = df.pop('date')
+df.insert(0, 'date', dateColumn)
+
+df.rename(columns = { "B_win_by_KO/TKO": "B_win_by_KO_TKO", "R_win_by_KO/TKO": "R_win_by_KO_TKO"}, inplace=True)
+
+print(df.describe())
+
+categorical_data = list(df.select_dtypes(include=["object"]))
+numeric_data = df.columns.tolist()
+for x in categorical_data:
+    numeric_data.remove(x)
+print(f"\nCategoricals {len(categorical_data)}")
+print(f"# Numerics {len(numeric_data)}")
+
+df = df.dropna()
+
+# missing data
+missing = round(df.isnull().sum()/df.shape[0]*100,2)
+print(f"\n% Missing in {len(missing[missing > 0])} Features:\n{missing[missing > 0]}")
+
+# reorder columns
+B_age = df.pop("B_age")
+df.insert(27, "B_age", B_age)	
+
+df["Winner"] = df["Winner"].replace(["Red", "Blue"], [0,1]).values # 0 = Red Winner | 1 = Blue Winner
+
+# shuffle data
+df = df.sample(frac=1, random_state=SEED).reset_index(drop=True)
+
+# separate 5% as test set
+test_rows = np.arange(0,0.05*len(df.index))
+ufc_test = df.iloc[test_rows].reset_index(drop=True)
+
+# remaining as training set for models
+ufc_train = df.drop(test_rows, inplace=False, axis=0) # remove test set from dataset
+ufc_train.reset_index(drop=1, inplace=True)
+
+print(f"df shape: {df.shape}")
+print(f"UFC_TEST shape: {ufc_test.shape}")
+print(f"UFC_TRAIN shape: {ufc_train.shape}")
+print("----------------------")
+print(df.info())
+print("----------------------")
+df.head()
+
+# export datasets
+df.to_csv("datasets/UFC_processed.csv",index=False)
+ufc_test.to_csv("datasets/UFC_TEST.csv",index=False)
+ufc_train.to_csv("datasets/UFC_TRAIN.csv",index=False) 
